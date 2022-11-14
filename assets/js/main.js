d7c301c7c6c40b742ceedc447075662a4450d532
dandevuk <dan@danhall.uk>
2022-11-14 23:50:57 +0000

    - add news section

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,3 +61,64 @@ links.forEach((link) => {
         link_title.innerHTML = '';
     });
 })
+
+// DATE COUNTER
+
+// function counter(time)
+
+// setInterval(function() {
+//     var date = new Date("2021-03-31T09:00:00.000Z");
+//     var start_time = date.getTime() / 1000;
+
+//     var current_date = new Date();
+//     var now = current_date.getTime() / 1000;
+
+//     var time_since = now - start_time;
+
+//     var secs_in_day = 60 * 60 * 1000 * 24;
+//     var   secs_in_hour = 60 * 60 * 1000;
+        
+//     var days = Math.floor(time_since / (secs_in_day) * 1);
+//     var hours = Math.floor((time_since % (secs_in_day)) / (secs_in_hour) * 1);
+//     var mins = Math.floor(((time_since % (secs_in_day)) % (secs_in_hour)) / (60 * 1000) * 1);
+//     var secs = Math.floor((((time_since % (secs_in_day)) % (secs_in_hour)) % (60 * 1000)) / 1000 * 1);
+
+    
+//     console.log("date",secs)
+
+//     document.querySelector('#days').innerHTML = days;
+//     document.querySelector('#hours').innerHTML = hours;
+//     document.querySelector('#mins').innerHTML = mins;
+//     document.querySelector('#secs').innerHTML = secs;
+// }, 1000);
+
+window.onload = function() {
+    // Month Day, Year Hour:Minute:Second, id-of-element-container
+    countUpFromTime("Mar 31, 2021 09:00:00", 'countup1'); // ****** Change this line!
+};
+
+function countUpFromTime(countFrom, id) {
+    countFrom = new Date(countFrom).getTime();
+    var now = new Date(),
+        countFrom = new Date(countFrom),
+        timeDifference = (now - countFrom);
+        
+    var secondsInADay = 60 * 60 * 1000 * 24,
+        secondsInAHour = 60 * 60 * 1000;
+        
+    days = Math.floor(timeDifference / (secondsInADay) * 1);
+    years = Math.floor(days / 365);
+    if (years > 1){ days = days - (years * 365) }
+    hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
+    mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
+    secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+
+    document.querySelector('#years').innerHTML = years;
+    document.querySelector('#days').innerHTML = days;
+    document.querySelector('#hours').innerHTML = hours;
+    document.querySelector('#mins').innerHTML = mins;
+    document.querySelector('#secs').innerHTML = secs;
+
+    clearTimeout(countUpFromTime.interval);
+    countUpFromTime.interval = setTimeout(function(){ countUpFromTime(countFrom, id); }, 1000);
+}
